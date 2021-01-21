6f4b5a75fde80f74d7832f27e64aa9d7a26d1c03
70xH <70xH@tutanota.com>
2021-01-21 17:59:47 +0000

    introduced menu

--- kuantum.go
+++ kuantum.go
@@ -1,8 +1,35 @@
 package kuantum
 
+import (
+	"os"
+
+	"github.com/rivo/tview"
+)
+
 func Kuantum() {
 
-	// tui for creating the VM
-	CreateTUI()
-	WriteTUI()
+	// new app
+	app := tview.NewApplication()
+
+	// choice
+	choice := tview.NewForm().
+		AddButton("New VM", func () {
+			app.Stop()
+			CreateTUI()
+			WriteTUI()
+		}).
+		AddButton("Existing VMs", nil).
+		AddButton("Write to an existing vm", nil).
+		AddButton("Run VM", nil).
+		AddButton("Running VMs", nil).
+		AddButton("Help", nil).
+		AddButton("Quit", func() {
+			app.Stop()
+			os.Exit(0)
+		})
+
+	choice.SetBorder(true).SetTitle("Menu").SetTitleAlign(tview.AlignLeft)
+	if err := app.SetRoot(choice, true).SetFocus(choice).Run(); err != nil {
+		panic(err)
+	}
 }
